8c31f743511a006214ff412cf8c4d924b4d4879e
unknown <Julie@PO001.infochallenge.info>
2015-10-18 04:59:05 +0800

    suppression ligne mobile

--- parts/common-head.php
+++ parts/common-head.php
@@ -28,5 +28,3 @@
 
 <link rel="stylesheet" type="text/css" href="lib/bootstrap-3.3.5-dist/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
-<link rel="stylesheet" media="screen and (max-device-width:1024px)" type="text/css" href="css/css600.css">
-<link rel="stylesheet" media="screen and (max-device-width:400px)" type="text/css" href="css/mobile.css">
