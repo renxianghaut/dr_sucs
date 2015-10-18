aa0d24df4f8989aec48290041aa902ee5044e8e5
soldiertt <soldiertt@gmail.com>
2015-10-18 22:43:15 +0200

    fix social links

--- parts/footer.php
+++ parts/footer.php
@@ -1,13 +1,16 @@
 <footer class="row">
+    <div class="col-md-3">
+
+    </div>
     <div class="col-md-6">
         <p>@2015 DR.SUCS | All right reserved<br/>23 avenue Emile De Mot ,1000 Bruxelles</p>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-3">
         <div class="social-link social-link-gplus">
-            Google +
+            <a href="https://plus.google.com/114808454276143550548" target="_blank">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Google +</a>
         </div>
-        <div class="social-link social-link-facebook">
+        <!--<div class="social-link social-link-facebook">
             Facebook
-        </div>
+        </div>-->
     </div>
 </footer>
--- parts/social.php
+++ parts/social.php
@@ -1,4 +1,4 @@
 <div id="fixedsocial">
-    <div class="sociallink facebookflat"><a href="https://plus.google.com/114808454276143550548" target="_blank"></a></div>
+    <!--<div class="sociallink facebookflat"><a href="https://plus.google.com/114808454276143550548" target="_blank"></a></div>-->
     <div class="sociallink gplusflat"><a href="https://plus.google.com/114808454276143550548" target="_blank"></a></div>
 </div>
